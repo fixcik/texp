290ecf39271d9c34c706ba76f2b5d19f38e9436f
Ivan Chirkin <chirkin.ivan@gmail.com>
2024-04-25 13:21:49 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::{fs::File, path::PathBuf};
+
+use anyhow::{anyhow, Context};
+use clap::Parser;
+use serde_yaml::Mapping;
+
+#[derive(Parser, Debug)]
+#[command(name = "texp")]
+#[command(bin_name = "texp")]
+#[command(version, about)]
+struct TextCli {
+    #[arg(name = "path")]
+    path: PathBuf,
+
+    #[arg(short, long)]
+    values: Option<PathBuf>,
+
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = TextCli::parse();
+    let mut tera = tera::Tera::default();
+    let mut context = tera::Context::new();
+
+    let values: Mapping = if let Some(path) = cli.values {
+        let reader = File::open(path)?;
+        serde_yaml::from_reader(reader)?
+    } else {
+        Default::default()
+    };
+
+    for (key, value) in &values {
+        context.insert(
+            key.as_str()
+                .ok_or(anyhow!("Key of values should be a string"))?,
+            value,
+        );
+    }
+
+    tera.add_template_file(cli.path, Some("template"))?;
+
+    if let Some(path) = cli.output {
+        let file = std::fs::File::create(path).context("Failed to create file")?;
+        tera.render_to("template", &context, file)?;
+    } else {
+        println!("{}", tera.render("template", &context)?)
+    }
+
+    Ok(())
+}
